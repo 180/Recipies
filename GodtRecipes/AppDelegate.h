0e8fee4261423aed6f1037f82b7173566bda588e
Maciej Stanik <M.Stanik@kainos.com>
2016-02-08 15:58:53 +0100

    init

---
+++ GodtRecipes/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GodtRecipes
+//
+//  Created by Maciej Stanik on 08/02/2016.
+//  Copyright © 2016 Maciej Stanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GodtRecipes/RecipesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipesViewController.h
+//  GodtRecipes
+//
+//  Created by Maciej Stanik on 08/02/2016.
+//  Copyright © 2016 Maciej Stanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipesViewController : UITableViewController
+
+@end
